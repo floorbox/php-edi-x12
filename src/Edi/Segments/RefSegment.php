25bfb6c0fcf1dc86138b3ca1583a740c54147a5b
Gabriel Vendrame-Piche <gabriel_vpiche@hotmail.com>
2025-03-13 11:47:25 -0400

    Add Quantity to Ref segment

--- src/Edi/Segments/RefSegment.php
+++ src/Edi/Segments/RefSegment.php
@@ -14,5 +14,6 @@ class RefSegment extends Segment
         0 => parent::EDI_QUALIFIER_KEY, //Reference Identification - To specify identifying information
         1 => 'reference_qualifier', //Reference Identification Qualifier - Code qualifying the Reference Identification
         2 => 'reference_value', //Reference Identification - Reference information as defined for a particular Transaction Set or as specified by the Reference Identification Qualifier
+        3 => 'quantity', //Quantity - Number of units of a commodity or item
     ];
 }
